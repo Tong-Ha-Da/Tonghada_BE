939de7de057d04705443cbac84cac35117929a71
mahmunsen <tjs339@gmail.com>
2023-10-30 23:10:32 +0900

    feat: test commit

---
+++ src/main/java/com/github/tonghada/TongHaDaApplication.java
@@ -0,0 +1,13 @@
+package com.github.tonghada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongHaDaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TongHaDaApplication.class, args);
+    }
+
+}
